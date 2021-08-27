b94040f417448961b874773f11410fa186c09355
Jennifer Law <jenniferlaw@Jennifers-MacBook-Air-2.local>
2021-08-26 19:12:29 -0700

    code refactoring and added unit test

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,22 +1,25 @@
 const request = require("request");
-const userInput = process.argv.slice(2);
-const breedInput = userInput[0];
 
-request(
-  `https://api.thecatapi.com/v1/breeds/search?q=${breedInput}`,
-  (error, response, body) => {
-    if (error) {
-      console.log("error:", error);
-      console.log("statusCode:", response && response.statusCode);
-    }
-    try {
+const fetchBreedDescription = function (breedName, callback) {
+  request(
+    `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`,
+    (error, response, body) => {
+      if (error) {
+        return callback(error);
+      }
+
       const data = JSON.parse(body);
-      console.log(data[0].description);
-    } catch (err) {
-      console.log("error: breed name not found");
-    }
-  }
-);
+      const breed = data[0];
 
+      if (breed) {
+        return callback(null, breed.description);
+      } else {
+        return callback("missing breed", null);
+      }
+    }
+  );
+};
+
+module.exports = { fetchBreedDescription };
 //> node breedFetcher.js Chartreux
 //The Chartreux is generally silent but communicative. Short play sessions, mixed
